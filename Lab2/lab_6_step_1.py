878ab36343566d3a5dd1a4d46f30bcb5f747b597
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2022-12-21 14:43:46 -0800

    Added more files

---
+++ Lab2/lab_6_step_1.py
@@ -0,0 +1,12 @@
+fruit = ["apples", "oranges", "bananas"]
+
+for item in fruit:
+    print(f"The best fruit is now {item}")
+
+numbers = [0,1,2,3,4,5,6,7,8,9,10]
+
+for number in numbers:
+    print(f"The next number is {number}")
+
+for number in range(0, 31, 3):
+    print(f"The next number is {number}")
